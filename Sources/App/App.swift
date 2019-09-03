9781ecc94d3b45d9800dfd041705a9381dd46c25
YutoMizutani <yuto.mizutani.dev@gmail.com>
2019-09-04 01:16:15 +0900

    :sparkles: Update responses

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -9,11 +9,12 @@ import Foundation
 
 /// Model describing a specific `App` in the Bitrise system
 public struct App: Decodable {
-    
-    // Unique Identifer of an app
-    public var slug: String
-    // The title of an app
-    public var title: String
+
+    public var avatarUrl: URL
+    // If this app is disabled
+    public var isDisabled: Bool
+    public var isPublic: Bool
+    public var owner: Owner
     // ios, android etc,
     // Todo: - Make an enum if we defined list of project types
     public var projectType: String
@@ -21,22 +22,42 @@ public struct App: Decodable {
     public var provider: String
     // Name of the owner of the repo
     public var repoOwner: String
-    // The repo url where the source code can be found
-    public var repoUrl: URL
     // A name of the repo without the owner namespace `MyCoolApp`
     public var repoSlug: String
-    // If this app is disabled
-    public var isDisabled: Bool
-    
+    // The repo url where the source code can be found
+    public var repoUrl: URL
+    // Unique Identifer of an app
+    public var slug: String
+    public var status: Int
+    // The title of an app
+    public var title: String
 
-    enum CodingKeys: String, CodingKey {
-        case slug
-        case title
+
+    public struct Owner: Codable {
+
+        public var accountType: String
+        public var name: String
+        public var slug: String
+
+        private enum CodingKeys: String, CodingKey {
+            case accountType = "account_type"
+            case name
+            case slug
+        }
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+        case isDisabled = "is_disabled"
+        case isPublic = "is_public"
+        case owner
         case projectType = "project_type"
         case provider
         case repoOwner = "repo_owner"
-        case repoUrl = "repo_url"
         case repoSlug = "repo_slug"
-        case isDisabled = "is_disabled"
+        case repoUrl = "repo_url"
+        case slug
+        case status
+        case title
     }
 }
